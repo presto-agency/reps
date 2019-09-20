c643f892fed97c0fa60580539955383398867fd8
Sergey <anyshyn@gmail.com>
2019-09-20 17:07:02 +0300

    SLEEP-Admin install

---
+++ app/Admin/bootstrap.php
@@ -0,0 +1,4 @@
+<?php
+
+// PackageManager::load('admin-default')
+//    ->css('extend', public_path('packages/sleepingowl/default/css/extend.css'));
---
+++ app/Providers/AdminSectionsServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Providers;
+
+use SleepingOwl\Admin\Providers\AdminSectionsServiceProvider as ServiceProvider;
+
+class AdminSectionsServiceProvider extends ServiceProvider
+{
+
+    /**
+     * @var array
+     */
+    protected $sections = [
+        //\App\User::class => 'App\Http\Sections\Users',
+    ];
+
+    /**
+     * Register sections.
+     *
+     * @param \SleepingOwl\Admin\Admin $admin
+     * @return void
+     */
+    public function boot(\SleepingOwl\Admin\Admin $admin)
+    {
+    	//
+
+        parent::boot($admin);
+    }
+}
