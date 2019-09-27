e885e0bee8c71d6438a0eb9a885355a9c718491b
NeoMaximus666 <maximgreenyuk@gmail.com>
2019-09-27 08:33:51 +0300

    #36 add sections ChatPicture

---
+++ app/Http/Sections/ChatPicture.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Sections;
+
+use SleepingOwl\Admin\Contracts\Display\DisplayInterface;
+use SleepingOwl\Admin\Contracts\Form\FormInterface;
+use SleepingOwl\Admin\Section;
+
+/**
+ * Class ChatPicture
+ *
+ * @property \App\Models\ChatPicture $model
+ *
+ * @see http://sleepingowladmin.ru/docs/model_configuration_section
+ */
+class ChatPicture extends Section
+{
+    /**
+     * @see http://sleepingowladmin.ru/docs/model_configuration#ограничение-прав-доступа
+     *
+     * @var bool
+     */
+    protected $checkAccess = false;
+
+    /**
+     * @var string
+     */
+    protected $title;
+
+    /**
+     * @var string
+     */
+    protected $alias;
+
+    /**
+     * @return DisplayInterface
+     */
+    public function onDisplay()
+    {
+        // remove if unused
+    }
+
+    /**
+     * @param int $id
+     *
+     * @return FormInterface
+     */
+    public function onEdit($id)
+    {
+        // remove if unused
+    }
+
+    /**
+     * @return FormInterface
+     */
+    public function onCreate()
+    {
+        return $this->onEdit(null);
+    }
+
+    /**
+     * @return void
+     */
+    public function onDelete($id)
+    {
+        // remove if unused
+    }
+
+    /**
+     * @return void
+     */
+    public function onRestore($id)
+    {
+        // remove if unused
+    }
+}
