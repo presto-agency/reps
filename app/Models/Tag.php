c141eb8ab286f9656ab232c6567aca962758162a
NeoMaximus666 <maximgreenyuk@gmail.com>
2019-09-27 17:59:04 +0300

    #36 modify tags

---
+++ app/Models/Tag.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Tag extends Model
+{
+    protected $fillable = [
+        'name', 'display_name',
+    ];
+    public function chatPictures()
+    {
+        return $this->belongsToMany('App\Models\ChatPicture','chat_picture_tag', 'tag_id','chat_picture_id');
+    }
+}
