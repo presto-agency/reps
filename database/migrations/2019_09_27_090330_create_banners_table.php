58c22fdba3f7858f0600c6949a51921f473a693c
NeoMaximus666 <maximgreenyuk@gmail.com>
2019-09-27 12:52:19 +0300

    #41 crud banners

---
+++ database/migrations/2019_09_27_090330_create_banners_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBannersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('banners', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title')     ->nullable();
+            $table->string('image');
+            $table->string('url_redirect');
+            $table->boolean('is_active')->default(1);
+            $table->timestamps();
+
+            $table->index('is_active');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('banners');
+    }
+}
