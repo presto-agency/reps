43dbe3a62903c33708d4f775880e08b99c03d40b
NeoMaximus666 <maximgreenyuk@gmail.com>
2019-09-30 17:28:03 +0300

    #45 add custom column

--- app/Http/Sections/ForumSections.php
+++ app/Http/Sections/ForumSections.php
@@ -49,9 +49,9 @@ class ForumSections extends Section
             ->setDatatableAttributes(['bInfo' => false])
 //            ->setDisplaySearch(true)
             ->setHtmlAttribute('class', 'table-info table-hover text-center')
-            /*->setActions([
+            ->setActions([
                 AdminColumn::action('export', 'Export')->setIcon('fa fa-share')->setAction('#'),
-            ])*/
+            ])
             ->paginate(50);
         $display->setApply(function ($query) {
             $query->orderBy('created_at', 'asc');
@@ -68,6 +68,9 @@ class ForumSections extends Section
                 ->setWidth('50px'),
             $description = AdminColumn::text('description', 'Description')
                 ->setWidth('50px'),
+            $quantity = AdminColumn::custom('Quantity topics', function(\Illuminate\Database\Eloquent\Model $model) {
+                return $model->topics()->count();
+            })->setWidth('50px'),
             $isActive = AdminColumnEditable::checkbox('is_active','Yes', 'No')
                 ->setLabel('Active'),
             $isGeneral = AdminColumnEditable::checkbox('is_general','Yes', 'No')
