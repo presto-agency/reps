dca93dbde450ab818cf927fd916536ffaf131da9
Savitska <savitskaoo9@ukr.net>
2019-10-01 17:33:13 +0300

    header+homePage

---
+++ resources/views/components/block_replay.blade.php
@@ -0,0 +1,22 @@
+<section class="block_replay">
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-3">
+                <div class="wrapper">
+                    <div class="title_replay">
+                        <p class="title_replayText">РЕПЛЕИ</p>
+                    </div>
+                    <div class="row row_game">
+                        <div class="col-xl-6">
+                            <p class="name_game">Busi v Last</p>
+                        </div>
+                        <div class="col-xl-6">
+                            <p class="name_game">BW Jeez weekly</p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
---
+++ resources/views/components/block_tournament.blade.php
@@ -0,0 +1,18 @@
+<section class="block_tournament">
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-3">
+                <div class="wrapper">
+                    <div class=" title_block">
+                        <img  src="{{url('images\svg\trophy-solid.svg')}}"/>
+                        <p class="title_text">Предстоящие турниры</p>
+                    </div>
+                    <div class="block_futureTournament">
+                        <p class="information_text">Нет предстоящих турниров</p>
+                        <button>ВСУ ТУРНИРЫ</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
