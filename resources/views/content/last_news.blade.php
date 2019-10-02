492c95d8bb9207f47342bedfee194d5f3e6a06b0
captain <donvov1990@gmail.com>
2019-10-02 15:21:19 +0300

    move folders in scss, add breadcrumbs blade file

---
+++ resources/views/content/last_news.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="container">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores, nisi?ß</p>
+    </div>
+
+
+@endsection
