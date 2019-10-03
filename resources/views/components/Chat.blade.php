90ee61c388262e1af479719a73dc9965628ac1b0
Savitska <savitskaoo9@ukr.net>
2019-10-03 23:08:37 +0300

    add chat

--- resources/views/components/Chat.blade.php
+++ resources/views/components/Chat.blade.php
@@ -2,37 +2,84 @@
     <div class="container">
         <p class="text_support">Не можешь восстановить старую учетку? Збыл емейл? Пиши Русбрейну, востаановим!Идёт работа по слиянию <a href="#">reps.ru</a> и <a href="#">defiler.ru</a></p>
         <div class="row">
-            <div class="col-xl-3 wrapper">
-                <div class="row container_titleChat block_chat">
-                    <div class="col-xl-6 col-lg-6 col-md-6 content_left">
+            <div class="col-xl-3 wrapper block_chat">
+                <div class="row container_titleChat ">
+                    <div class="col-xl-6 col-lg-6 col-md-6 col-6 content_left">
                         <img id="img_menuMob" class="icon_bars" src="{{url('images\speech-bubble.png')}}"/>
                         <p class="title_Text">Guest</p>
                     </div>
-                    <div class="col-xl-6 col-lg-6 col-md-6 content_right">
+                    <div class="col-xl-6 col-lg-6 col-md-6 col-6 content_right">
                         <img class="icon_bars" src="{{url('images\arrow-point-to-right.png')}}"/>
                     </div>
                 </div>
-                <div class="row_contentChat">
-                    <div class=" block_user_akk">
-                        <img class="icon_bars" src="{{url('images\country_flag.png')}}"/>
-                        <img class="icon_bars" src="{{url('images\ava.png')}}"/>
-                        <span class="blue_text">Rus_Brain</span>
-                        <span>#3797</span>
-
-                        <div class=" block_close">
-                            <button>
-                                 <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="times" class="svg-inline--fa fa-times fa-w-11" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512"><path fill="#bfc3ce" d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"></path></svg>
-                            </button>
+                <div class="messanger">
+                    <div class="row_contentChat">
+                        <div class=" block_user_akk">
+                            <div class="user">
+                                <img class="icon_bars" src="{{url('images\country_flag.png')}}"/>
+                                <img class="icon_bars" src="{{url('images\ava.png')}}"/>
+                                <span class="blue_text">Rus_Brain</span>
+                                <span>#3797</span>
+                            </div>
+                            <div class=" block_close">
+                                <button>
+                                    <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="times" class="svg-inline--fa fa-times fa-w-11" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512"><path fill="#bfc3ce" d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"></path></svg>
+                                </button>
+                            </div>
+                            <div class=" block_date">
+                                <span>1.01</span>
+                            </div>
                         </div>
-                        <div class=" block_date">
-                            <span>1.01</span>
+                        <div class=" block_userMessage">
+                            <span class="user_nick">@45554</span>
+                            <span class="user_text">,но учитывая, что у нас разница год, на фоне десятилетней истории уже ничего не значит</span>
+                        </div>
+
+                    </div>
+                    <div class="row_contentChat">
+                        <div class=" block_user_akk">
+                            <div class="user">
+                                <img class="icon_bars" src="{{url('images\country_flag.png')}}"/>
+                                <img class="icon_bars" src="{{url('images\ava.png')}}"/>
+                                <span class="blue_text">Rus_Brain</span>
+                                <span>#3797</span>
+                            </div>
+                            <div class=" block_close">
+                                <button>
+                                    <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="times" class="svg-inline--fa fa-times fa-w-11" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512"><path fill="#bfc3ce" d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"></path></svg>
+                                </button>
+                            </div>
+                            <div class=" block_date">
+                                <span>1.01</span>
+                            </div>
+                        </div>
+                        <div class=" block_userMessage">
+                            <span class="user_nick">@45554</span>
+                            <span class="user_text">,но учитывая, что у нас разница год, на фоне десятилетней истории уже ничего не значит</span>
                         </div>
                     </div>
-                    <div class=" block_userMessage">
-                        <span class="user_nick">@45554</span>
-                        <span class="user_text">,но учитывая, что у нас разница год, на фоне десятилетней истории уже ничего не значит</span>
+                    <div class="row_contentChat">
+                        <div class=" block_user_akk">
+                            <div class="user">
+                                <img class="icon_bars" src="{{url('images\country_flag.png')}}"/>
+                                <img class="icon_bars" src="{{url('images\ava.png')}}"/>
+                                <span class="blue_text">Rus_Brain</span>
+                                <span>#3797</span>
+                            </div>
+                            <div class=" block_close">
+                                <button>
+                                    <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="times" class="svg-inline--fa fa-times fa-w-11" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512"><path fill="#bfc3ce" d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"></path></svg>
+                                </button>
+                            </div>
+                            <div class=" block_date">
+                                <span>1.01</span>
+                            </div>
+                        </div>
+                        <div class=" block_userMessage">
+                            <span class="user_nick">@45554</span>
+                            <span class="user_text">      <img class="icon_bars" src="{{url('images\Layer_6.png')}}"/></span>
+                        </div>
                     </div>
-
                 </div>
 
             </div>
