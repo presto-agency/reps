6936c0ee876c93a766f1a81588610ac5ad26162a
Sergey <anyshyn@gmail.com>
2019-10-03 11:36:46 +0300

    add show button controller views/add count with Relations

--- app/Models/InterviewVariantAnswer.php
+++ app/Models/InterviewVariantAnswer.php
@@ -2,11 +2,16 @@
 
 namespace App\Models;
 
+use App\Traits\ModelRelations\InterviewVariantAnswerRelationTrait;
 use Illuminate\Database\Eloquent\Model;
 
 class InterviewVariantAnswer extends Model
 {
+    use InterviewVariantAnswerRelationTrait;
+
     protected $fillable = [
         'question_id', 'answer',
     ];
+
+
 }
