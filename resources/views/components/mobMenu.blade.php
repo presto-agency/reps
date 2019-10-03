20970329d236fe1c10dfb85dc35e19ca29c473f0
Сергей Анишин <anyshyn@gmail.com>
2019-10-03 09:33:16 +0000

    Revert "Merge branch 'olga' into 'development'"
    This reverts merge request !33

--- resources/views/components/mobMenu.blade.php
+++
@@ -1,9 +0,0 @@
-<section class="mob_menu">
-    <a href="#" class="menu_off">x</a>
-    <ul>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-        <li>4</li>
-    </ul>
-</section>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,8 +11,7 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-{{--    <script defer src="{{ asset('js/jquery.maskedinput.min.js') }}"></script>--}}
+
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
@@ -21,19 +20,6 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-<div id="app">
-    @include('components.header')
-    @include('components.Chat-twitch')
-    @include('components.block_tournament')
-    @include('components.block_replay')
-    @include('components.block_lastNews')
-    @include('components.block_top')
-    <main class="py-4">
-        @yield('content')
-    </main>
-    @include('components.footer')
-
-</div>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
