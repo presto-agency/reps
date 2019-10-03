d49e8131dada2337483931bcdcf86b95cbf9fc8a
Savitska <savitskaoo9@ukr.net>
2019-10-03 13:04:24 +0300

    return back

---
+++ resources/views/components/block-replay.blade.php
@@ -0,0 +1,235 @@
+<section class="block_replay">
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-3 col-lg-3 col-md-6 col-12">
+                <div class="wrapper">
+                    <div class="title_replay">
+                        <p class="title_replayText">РЕПЛЕИ</p>
+                    </div>
+                    <div class="row row_game">
+                        <div class="col-xl-6 col-lg-6 col-md-6 col-12">
+                            <div class="game_oneSection">
+                                <p class="name_game" title="Busi v Last">Busi v Last</p>
+                                <div class="content_game">
+                                    <p class="text">Страны:</p>
+                                    <img class="icon_bars" src="{{url('images\country_flag.png')}}"/>
+                                    <p class="text">vs</p>
+                                    <img class="icon_bars" src="{{url('images\country_flag.png')}}"/>
+                                </div>
+                                <div class="content_game">
+                                    <p class="text">Матчап:</p>
+                                    <p class="text_matchap">Z</p>
+                                    <p class="text">vs</p>
+                                    <p class="text_matchap">P</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-6 col-lg-6 col-md-6 col-12">
+                            <div class="game_oneSection">
+                                <p class="name_game" title="BW Jeel weekly">BW Jeel weekly</p>
+                                <div class="content_game">
+                                    <p class="text">Матчап:</p>
+                                    <img class="icon_bars" src="{{url('images\country_flag.png')}}"/>
+                                    <p class="text">vs</p>
+                                    <img class="icon_bars" src="{{url('images\country_flag.png')}}"/>
+                                </div>
+                                <div class="content_game">
+                                    <p class="text">Страны:</p>
+                                    <p class="text_matchap">Z</p>
+                                    <p class="text">vs</p>
+                                    <p class="text_matchap">P</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row row_game">
+                        <div class="col-xl-6 col-lg-6 col-md-6 col-12">
+                            <div class="game_oneSection">
+                                <p class="name_game" title="Busi v Last">Busi v Last</p>
+                                <div class="content_game">
+                                    <p class="text">Страны:</p>
+                                    <img class="icon_bars" src="{{url('images\country_flag.png')}}"/>
+                                    <p class="text">vs</p>
+                                    <img class="icon_bars" src="{{url('images\country_flag.png')}}"/>
+                                </div>
+                                <div class="content_game">
+                                    <p class="text">Матчап:</p>
+                                    <p class="text_matchap">Z</p>
+                                    <p class="text">vs</p>
+                                    <p class="text_matchap">P</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-6 col-lg-6 col-md-6 col-12">
+                            <div class="game_oneSection">
+                                <p class="name_game" title="Busi v Last">Busi v Last</p>
+                                <div class="content_game">
+                                    <p class="text">Матчап:</p>
+                                    <img class="icon_bars" src="{{url('images\country_flag.png')}}"/>
+                                    <p class="text">vs</p>
+                                    <img class="icon_bars" src="{{url('images\country_flag.png')}}"/>
+                                </div>
+                                <div class="content_game">
+                                    <p class="text">Страны:</p>
+                                    <p class="text_matchap">Z</p>
+                                    <p class="text">vs</p>
+                                    <p class="text_matchap">P</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row row_game">
+                        <div class="col-xl-6 col-lg-6 col-md-6 col-12">
+                            <div class="game_oneSection">
+                                <p class="name_game" title="Busi v Last">Busi v Last</p>
+                                <div class="content_game">
+                                    <p class="text">Страны:</p>
+                                    <img class="icon_bars" src="{{url('images\country_flag.png')}}"/>
+                                    <p class="text">vs</p>
+                                    <img class="icon_bars" src="{{url('images\country_flag.png')}}"/>
+                                </div>
+                                <div class="content_game">
+                                    <p class="text">Матчап:</p>
+                                    <p class="text_matchap">Z</p>
+                                    <p class="text">vs</p>
+                                    <p class="text_matchap">P</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-6 col-lg-6 col-md-6 col-12">
+                            <div class="game_oneSection">
+                                <p class="name_game" title="Busi v Last">Busi v Last</p>
+                                <div class="content_game">
+                                    <p class="text">Матчап:</p>
+                                    <img class="icon_bars" src="{{url('images\country_flag.png')}}"/>
+                                    <p class="text">vs</p>
+                                    <img class="icon_bars" src="{{url('images\country_flag.png')}}"/>
+                                </div>
+                                <div class="content_game">
+                                    <p class="text">Страны:</p>
+                                    <p class="text_matchap">Z</p>
+                                    <p class="text">vs</p>
+                                    <p class="text_matchap">P</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row row_game">
+                        <div class="col-xl-6 col-lg-6 col-md-6 col-12">
+                            <div class="game_oneSection">
+                                <p class="name_game" title="Busi v Last">Busi v Last</p>
+                                <div class="content_game">
+                                    <p class="text">Страны:</p>
+                                    <img class="icon_bars" src="{{url('images\country_flag.png')}}"/>
+                                    <p class="text">vs</p>
+                                    <img class="icon_bars" src="{{url('images\country_flag.png')}}"/>
+                                </div>
+                                <div class="content_game">
+                                    <p class="text">Матчап:</p>
+                                    <p class="text_matchap">Z</p>
+                                    <p class="text">vs</p>
+                                    <p class="text_matchap">P</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-6 col-lg-6 col-md-6 col-12">
+                            <div class="game_oneSection">
+                                <p class="name_game" title="Busi v Last">Busi v Last</p>
+                                <div class="content_game">
+                                    <p class="text">Матчап:</p>
+                                    <img class="icon_bars" src="{{url('images\country_flag.png')}}"/>
+                                    <p class="text">vs</p>
+                                    <img class="icon_bars" src="{{url('images\country_flag.png')}}"/>
+                                </div>
+                                <div class="content_game">
+                                    <p class="text">Страны:</p>
+                                    <p class="text_matchap">Z</p>
+                                    <p class="text">vs</p>
+                                    <p class="text_matchap">P</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="container_btn">
+                        <button class="button">ВСЕ ТУРНИРЫ</button>
+                    </div>
+                    <div class="row row_game">
+                        <div class="col-xl-6 col-lg-6 col-md-6 col-12">
+                            <div class="game_oneSection">
+                                <p class="name_game" title="Busi v Last">Busi v Last</p>
+                                <div class="content_game">
+                                    <p class="text">Страны:</p>
+                                    <img class="icon_bars" src="{{url('images\country_flag.png')}}"/>
+                                    <p class="text">vs</p>
+                                    <img class="icon_bars" src="{{url('images\country_flag.png')}}"/>
+                                </div>
+                                <div class="content_game">
+                                    <p class="text">Матчап:</p>
+                                    <p class="text_matchap">Z</p>
+                                    <p class="text">vs</p>
+                                    <p class="text_matchap">P</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-6 col-lg-6 col-md-6 col-12">
+                            <div class="game_oneSection">
+                                <p class="name_game" title="Busi v Last">Busi v Last</p>
+                                <div class="content_game">
+                                    <p class="text">Матчап:</p>
+                                    <img class="icon_bars" src="{{url('images\country_flag.png')}}"/>
+                                    <p class="text">vs</p>
+                                    <img class="icon_bars" src="{{url('images\country_flag.png')}}"/>
+                                </div>
+                                <div class="content_game">
+                                    <p class="text">Страны:</p>
+                                    <p class="text_matchap">Z</p>
+                                    <p class="text">vs</p>
+                                    <p class="text_matchap">P</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row row_game">
+                        <div class="col-xl-6 col-lg-6 col-md-6 col-12">
+                            <div class="game_oneSection">
+                                <p class="name_game" title="Busi v Last">Busi v Last</p>
+                                <div class="content_game">
+                                    <p class="text">Страны:</p>
+                                    <img class="icon_bars" src="{{url('images\country_flag.png')}}"/>
+                                    <p class="text">vs</p>
+                                    <img class="icon_bars" src="{{url('images\country_flag.png')}}"/>
+                                </div>
+                                <div class="content_game">
+                                    <p class="text">Матчап:</p>
+                                    <p class="text_matchap">Z</p>
+                                    <p class="text">vs</p>
+                                    <p class="text_matchap">P</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-6 col-lg-6 col-md-6 col-12">
+                            <div class="game_oneSection">
+                                <p class="name_game" title="Busi v Last">Busi v Last</p>
+                                <div class="content_game">
+                                    <p class="text">Матчап:</p>
+                                    <img class="icon_bars" src="{{url('images\country_flag.png')}}"/>
+                                    <p class="text">vs</p>
+                                    <img class="icon_bars" src="{{url('images\country_flag.png')}}"/>
+                                </div>
+                                <div class="content_game">
+                                    <p class="text">Страны:</p>
+                                    <p class="text_matchap">Z</p>
+                                    <p class="text">vs</p>
+                                    <p class="text_matchap">P</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="container_btn">
+                        <button class="button">ВСЕ ТУРНИРЫ</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
