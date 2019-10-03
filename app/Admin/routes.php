cdb531f91ffb59be7bb4f03a55b1d9850cb8d01b
NeoMaximus666 <maximgreenyuk@gmail.com>
2019-10-03 12:54:30 +0300

    #67 add crud comments

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -13,3 +13,6 @@ Route::get('information', ['as' => 'information', function () {
     $content = 'Define your information here.';
     return AdminSection::view($content, 'Information');
 }]);
+
+Route::get('forum_topics/show/{id}', '\App\Http\Controllers\Admin\ForumTopicController@show');
+Route::post('forum_topics/show/{id}/send_comment', '\App\Http\Controllers\Admin\TopicCommentController@store')->name('admin.forum.topic.comment_send');
