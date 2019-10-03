e4d5287e07728bdfbd270c13e38465436b999e73
Savitska <savitskaoo9@ukr.net>
2019-10-03 18:04:13 +0300

    add chat

--- resources/views/components/Chat.blade.php
+++ resources/views/components/Chat.blade.php
@@ -1,25 +1,73 @@
 <section class="block_chat-twitch">
     <div class="container">
-            <p class="text_support">Не можешь восстановить старую учетку? Збыл емейл? Пиши Русбрейну, востаановим!Идёт работа по слиянию <a href="#">reps.ru</a> и <a href="#">defiler.ru</a></p>
-            <div class="row">
-                <div class="col-xl-3 col-lg-3 col-md-3 block_chat">
-                    <div class="row row_titleChat">
-                        <div class="col-xl-6 col-lg-6 col-md-6 content_left">
-                            <img id="img_menuMob" class="icon_bars" src="{{url('images\speech-bubble.png')}}"/>
-                            <p class="titleGuest">Guest</p>
-                        </div>
-                        <div class="col-xl-6 col-lg-6 col-md-6 content_right">
-                            <img class="icon_bars" src="{{url('images\arrow-point-to-right.png')}}"/>
-                        </div>
+        <p class="text_support">Не можешь восстановить старую учетку? Збыл емейл? Пиши Русбрейну, востаановим!Идёт работа по слиянию <a href="#">reps.ru</a> и <a href="#">defiler.ru</a></p>
+        <div class="row">
+            <div class="col-xl-3 wrapper">
+                <div class="row container_titleChat block_chat">
+                    <div class="col-xl-6 col-lg-6 col-md-6 content_left">
+                        <img id="img_menuMob" class="icon_bars" src="{{url('images\speech-bubble.png')}}"/>
+                        <p class="title_Text">Guest</p>
+                    </div>
+                    <div class="col-xl-6 col-lg-6 col-md-6 content_right">
+                        <img class="icon_bars" src="{{url('images\arrow-point-to-right.png')}}"/>
                     </div>
                 </div>
-                <div class="col-xl-9 col-lg-9 col-md-9 title_video">
-                    <div class="video_header">
+                <div class="row_contentChat">
+                    <div class=" block_user_akk">
                         <img class="icon_bars" src="{{url('images\country_flag.png')}}"/>
                         <img class="icon_bars" src="{{url('images\ava.png')}}"/>
+                        <span class="blue_text">Rus_Brain</span>
+                        <span>#3797</span>
+
+                        <div class=" block_close">
+                            <button>
+                                 <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="times" class="svg-inline--fa fa-times fa-w-11" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512"><path fill="#bfc3ce" d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"></path></svg>
+                            </button>
+                        </div>
+                        <div class=" block_date">
+                            <span>1.01</span>
+                        </div>
                     </div>
+                    <div class=" block_userMessage">
+                        <span class="user_nick">@45554</span>
+                        <span class="user_text">,но учитывая, что у нас разница год, на фоне десятилетней истории уже ничего не значит</span>
+                    </div>
+
+                </div>
+
+            </div>
+            <div class="col-xl-9 col-lg-9 col-md-9 title_video">
+                <div class="video_header">
+                    <img class="icon_bars" src="{{url('images\country_flag.png')}}"/>
+                    <img class="icon_bars" src="{{url('images\ava.png')}}"/>
+                    <p class="title_Text">Guest</p>
                 </div>
             </div>
         </div>
     </div>
 </section>
+{{--    <div class="container">--}}
+{{--        <p class="text_support">Не можешь восстановить старую учетку? Збыл емейл? Пиши Русбрейну, востаановим!Идёт работа по слиянию <a href="#">reps.ru</a> и <a href="#">defiler.ru</a></p>--}}
+{{--        <div class="row">--}}
+{{--            <div class="col-xl-3 col-lg-3 col-md-3 block_chat">--}}
+{{--                <div class="row row_titleChat">--}}
+{{--                    <div class="col-xl-6 col-lg-6 col-md-6 content_left">--}}
+{{--                        <img id="img_menuMob" class="icon_bars" src="{{url('images\speech-bubble.png')}}"/>--}}
+{{--                        <p class="titleGuest">Guest</p>--}}
+{{--                    </div>--}}
+{{--                    <div class="col-xl-6 col-lg-6 col-md-6 content_right">--}}
+{{--                        <img class="icon_bars" src="{{url('images\arrow-point-to-right.png')}}"/>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--                <div >5454</div>--}}
+{{--            </div>--}}
+{{--            <div class="col-xl-9 col-lg-9 col-md-9 title_video">--}}
+{{--                <div class="video_header">--}}
+{{--                    <img class="icon_bars" src="{{url('images\country_flag.png')}}"/>--}}
+{{--                    <img class="icon_bars" src="{{url('images\ava.png')}}"/>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+
+{{--    </div>--}}
+
