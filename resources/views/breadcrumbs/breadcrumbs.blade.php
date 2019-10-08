c9e4a1936de0a40dbfa262862866b219641ad716
captain <donvov1990@gmail.com>
2019-10-08 16:18:07 +0300

    add and complete breadcrumbs;

---
+++ resources/views/breadcrumbs/breadcrumbs.blade.php
@@ -0,0 +1,7 @@
+<nav aria-label="breadcrumb">
+    <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="#">Главная</a></li>
+        <li class="breadcrumb-item"><a href="#">Лучшие</a></li>
+        <li class="breadcrumb-item active" aria-current="page">Cup Qualification Russia #2</li>
+    </ol>
+</nav>
