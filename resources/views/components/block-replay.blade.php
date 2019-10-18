259d7e298ba3f0dc0ef9f148c3d5a309de85e03f
captain <donvov1990@gmail.com>
2019-10-18 17:53:19 +0300

    laptop adaptive

--- resources/views/components/block-replay.blade.php
+++ resources/views/components/block-replay.blade.php
@@ -4,7 +4,7 @@
             <p class="title_Text">РЕПЛЕИ</p>
         </div>
         <div class="row row_game">
-            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12 pl-0 pr-0">
                 <div class="game_oneSection">
                     <a class="name_game" title="Busi v Last"><p>Busi v Last</p></a>
                     <div class="content_game">
@@ -21,7 +21,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12 pl-0 pr-0">
                 <div class="game_oneSection">
                     <a class="name_game" title="BW Jeel weekly"><p>BW Jeel weekly</p></a>
                     <div class="content_game">
@@ -40,7 +40,7 @@
             </div>
         </div>
         <div class="row row_game">
-            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12 pl-0 pr-0">
                 <div class="game_oneSection">
                     <a class="name_game" title="Busi v Last"><p>Busi v Last</p></a>
                     <div class="content_game">
@@ -57,43 +57,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
-                <div class="game_oneSection">
-                    <a class="name_game" title="Busi v Last"><p>Busi v Last</p></a>
-                    <div class="content_game">
-                        <p class="text">Страны:</p>
-                        <img class="icon_bars" src="{{url('images\country_flag.png')}}"/>
-                        <p class="text">vs</p>
-                        <img class="icon_bars" src="{{url('images\country_flag.png')}}"/>
-                    </div>
-                    <div class="content_game">
-                        <p class="text">Матчап:</p>
-                        <p class="text_matchap">Z</p>
-                        <p class="text">vs</p>
-                        <p class="text_matchap">P</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="row row_game">
-            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
-                <div class="game_oneSection">
-                    <a class="name_game" title="Busi v Last"><p>Busi v Last</p></a>
-                    <div class="content_game">
-                        <p class="text">Страны:</p>
-                        <img class="icon_bars" src="{{url('images\country_flag.png')}}"/>
-                        <p class="text">vs</p>
-                        <img class="icon_bars" src="{{url('images\country_flag.png')}}"/>
-                    </div>
-                    <div class="content_game">
-                        <p class="text">Матчап:</p>
-                        <p class="text_matchap">Z</p>
-                        <p class="text">vs</p>
-                        <p class="text_matchap">P</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12 pl-0 pr-0">
                 <div class="game_oneSection">
                     <a class="name_game" title="Busi v Last"><p>Busi v Last</p></a>
                     <div class="content_game">
@@ -112,7 +76,7 @@
             </div>
         </div>
         <div class="row row_game">
-            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12 pl-0 pr-0">
                 <div class="game_oneSection">
                     <a class="name_game" title="Busi v Last"><p>Busi v Last</p></a>
                     <div class="content_game">
@@ -129,7 +93,43 @@
                     </div>
                 </div>
             </div>
-            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12 pl-0 pr-0">
+                <div class="game_oneSection">
+                    <a class="name_game" title="Busi v Last"><p>Busi v Last</p></a>
+                    <div class="content_game">
+                        <p class="text">Страны:</p>
+                        <img class="icon_bars" src="{{url('images\country_flag.png')}}"/>
+                        <p class="text">vs</p>
+                        <img class="icon_bars" src="{{url('images\country_flag.png')}}"/>
+                    </div>
+                    <div class="content_game">
+                        <p class="text">Матчап:</p>
+                        <p class="text_matchap">Z</p>
+                        <p class="text">vs</p>
+                        <p class="text_matchap">P</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row row_game">
+            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12 pl-0 pr-0">
+                <div class="game_oneSection">
+                    <a class="name_game" title="Busi v Last"><p>Busi v Last</p></a>
+                    <div class="content_game">
+                        <p class="text">Страны:</p>
+                        <img class="icon_bars" src="{{url('images\country_flag.png')}}"/>
+                        <p class="text">vs</p>
+                        <img class="icon_bars" src="{{url('images\country_flag.png')}}"/>
+                    </div>
+                    <div class="content_game">
+                        <p class="text">Матчап:</p>
+                        <p class="text_matchap">Z</p>
+                        <p class="text">vs</p>
+                        <p class="text_matchap">P</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12 pl-0 pr-0">
                 <div class="game_oneSection">
                     <a class="name_game" title="Busi v Last"><p>Busi v Last</p></a>
                     <div class="content_game">
@@ -151,7 +151,7 @@
             <button class="button button__download-more">Другие госу реплеи</button>
         </div>
         <div class="row row_game">
-            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12 pl-0 pr-0">
                 <div class="game_oneSection">
                     <a class="name_game" title="Busi v Last"><p>Busi v Last</p></a>
                     <div class="content_game">
@@ -168,7 +168,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12 pl-0 pr-0">
                 <div class="game_oneSection">
                     <a class="name_game" title="Busi v Last"><p>Busi v Last</p></a>
                     <div class="content_game">
@@ -187,7 +187,7 @@
             </div>
         </div>
         <div class="row row_game">
-            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12 pl-0 pr-0">
                 <div class="game_oneSection">
                     <a class="name_game" title="Busi v Last"><p>Busi v Last</p></a>
                     <div class="content_game">
@@ -204,7 +204,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12 pl-0 pr-0">
                 <div class="game_oneSection">
                     <a class="name_game" title="Busi v Last"><p>Busi v Last</p></a>
                     <div class="content_game">
