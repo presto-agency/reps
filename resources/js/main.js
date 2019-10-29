30c061b19c175108218dd7a975eaf0841eafac4b
Savitska <savitskaoo9@ukr.net>
2019-10-29 14:32:15 +0200

    mobile repytation-info

--- resources/js/main.js
+++ resources/js/main.js
@@ -83,19 +83,28 @@ $('.big_video_left').click(function(event) {
 
      });
  });
-
-
+if(localStorage.getItem('nightMode')==1){
+    $('body').addClass('nightMode');
+}
+else $('body').removeClass('nightMode');
 //day-night
 $('#night').click(function(event) {
     event.preventDefault();
     console.log("11");
     $('body').addClass('nightMode');
-    // localStorage.setItem('panelTheme', theme);
+     //localStorage.setItem('nightMode', theme);
+    localStorage.setItem('nightMode', 1);
+
     // $(':root').css('--blue', 'red');
     // document.documentElement.style.setProperty('--blue', 'green');
-    root.style.setProperty('--blue', '#f9f9f9');
 
 
+});
+$('#day').click(function(event) {
+    event.preventDefault();
+    console.log("21");
+    $('body').removeClass('nightMode');
+    localStorage.setItem('nightMode', 2);
 });
 
 /*accordion replays script   start*/
