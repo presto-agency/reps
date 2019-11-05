fae6f1f7584254eb69bd2dadf7fda2973492b709
Savitska <savitskaoo9@ukr.net>
2019-11-05 13:59:55 +0200

    REPS-195

--- resources/views/components/mobile_menu.blade.php
+++ resources/views/components/mobile_menu.blade.php
@@ -1,19 +1,28 @@
 <section class="mob_menu" >
     <nav class=" nav-wrapper" id="nav-mobile">
-        <div class="btn-round">
-            <span class="close"></span>
+        <div class="main_punkt">
+            <div class="btn-round">
+                <span class="close"></span>
+            </div>
+            <ul class="punct">
+                <li><a class="nav_item"  href="/">ГЛАВНАЯ</a></li>
+                <li><a class="nav_item" href="/forum">ФОРУМ</a></li>
+                <li><a class="nav_item" href="/replay">РЕПЛЕИ</a></li>
+                <li><a class="nav_item" href="/news">НОВОСТИ</a></li>
+                <li><a class="nav_item" href="/tournament">ТУРНИРЫ</a></li>
+                <li><a class="nav_item" href="/best">ЛУЧШИЕ</a></li>
+                <li><a class="nav_item" href="#">ФАЙЛЫ</a></li>
+            </ul>
         </div>
-        <input type="checkbox" id="nav" class="hidden">
-        <ul class="punct">
-            <li><a class="nav_item"  href="/">ГЛАВНАЯ</a></li>
-            <li><a class="nav_item" href="/forum">ФОРУМ</a></li>
-            <li><a class="nav_item" href="/replay">РЕПЛЕИ</a></li>
-            <li><a class="nav_item" href="/news">НОВОСТИ</a></li>
-            <li><a class="nav_item" href="/tournament">ТУРНИРЫ</a></li>
-            <li><a class="nav_item" href="/best">ЛУЧШИЕ</a></li>
-            <li><a class="nav_item" href="#">ФАЙЛЫ</a></li>
-        </ul>
-        <div class="bottom_menu"></div>
+
+        <div class="bottom_menu">
+            <button>
+                <p>registration</p>
+            </button>
+            <button>
+                <p>login</p>
+            </button>
+        </div>
     </nav>
 </section>
 
