5779271dd9e1ae6b65497ba86ab47495ecbffdaa
Sergey <anyshyn@gmail.com>
2019-11-08 19:09:40 +0200

    add logout

--- resources/views/layouts/components/header/components/user-bar-panel.blade.php
+++ resources/views/layouts/components/header/components/user-bar-panel.blade.php
@@ -18,7 +18,7 @@
                         d="M192 256c61.9 0 112-50.1 112-112S253.9 32 192 32 80 82.1 80 144s50.1 112 112 112zm76.8 32h-8.3c-20.8 10-43.9 16-68.5 16s-47.6-6-68.5-16h-8.3C51.6 288 0 339.6 0 403.2V432c0 26.5 21.5 48 48 48h288c26.5 0 48-21.5 48-48v-28.8c0-63.6-51.6-115.2-115.2-115.2zM480 256c53 0 96-43 96-96s-43-96-96-96-96 43-96 96 43 96 96 96zm48 32h-3.8c-13.9 4.8-28.6 8-44.2 8s-30.3-3.2-44.2-8H432c-20.4 0-39.2 5.9-55.7 15.4 24.4 26.3 39.7 61.2 39.7 99.8v38.4c0 2.2-.5 4.3-.6 6.4H592c26.5 0 48-21.5 48-48 0-61.9-50.1-112-112-112z"></path>
                 </svg>
             </a>
-            <a href="#" title="Мои друзья">
+            <a href="#" title="Мои сообщения">
                 <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px"
                      y="0px" viewBox="0 0 1000 1000" enable-background="new 0 0 1000 1000" xml:space="preserve">
                 <path
@@ -27,7 +27,8 @@
                         d="M647.9,755.4c-28,0-50.7,22.7-50.7,50.7v81.7H111.4V112.2h485.8v97.6c0,28,22.7,50.7,50.7,50.7c28,0,50.7-22.7,50.7-50.7V91.3c0-44.4-36.1-80.5-80.5-80.5H90.5C46.1,10.8,10,46.9,10,91.3v817.3c0,44.4,36.1,80.5,80.5,80.5H618c44.4,0,80.5-36.1,80.5-80.5V806.1C698.6,778.1,675.9,755.4,647.9,755.4z"/>
                 </svg>
             </a>
-            <a href="#" title="Мои друзья">
+            <a href="#" onclick="event.preventDefault(); document.getElementById('logout-form-home').submit();"
+               title="Выход">
                 <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px"
                      y="0px" viewBox="0 0 1000 1000" enable-background="new 0 0 1000 1000" xml:space="preserve">
                 <path
@@ -36,7 +37,10 @@
                         d="M647.9,755.4c-28,0-50.7,22.7-50.7,50.7v81.7H111.4V112.2h485.8v97.6c0,28,22.7,50.7,50.7,50.7c28,0,50.7-22.7,50.7-50.7V91.3c0-44.4-36.1-80.5-80.5-80.5H90.5C46.1,10.8,10,46.9,10,91.3v817.3c0,44.4,36.1,80.5,80.5,80.5H618c44.4,0,80.5-36.1,80.5-80.5V806.1C698.6,778.1,675.9,755.4,647.9,755.4z"/>
                 </svg>
             </a>
-            <a href="#" id='settings' title="Мои друзья">
+            <form id="logout-form-home" action="{{ route('logout') }}" method="POST" style="display: none;">
+                @csrf
+            </form>
+            <a href="#" id='settings' title="Настройки">
                 <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="cog"
                      class="svg-inline--fa fa-cog fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg"
                      viewBox="0 0 512 512">
@@ -56,10 +60,10 @@
                 <a href="{{route('user_profile',['id' => auth()->id()])}}"
                    title="Мой аккаунт">Мой аккаунт</a>
             </li>
-{{--            <li>--}}
-{{--                <a href="{{route('user-gallery.index',['id' => auth()->id()]) }}"--}}
-{{--                   title="Моя галерея">Моя галерея</a>--}}
-{{--            </li>--}}
+            {{--            <li>--}}
+            {{--                <a href="{{route('user-gallery.index',['id' => auth()->id()]) }}"--}}
+            {{--                   title="Моя галерея">Моя галерея</a>--}}
+            {{--            </li>--}}
             <li>
                 <a href="{{route('user-gallery.index',['id' => auth()->id()]) }}"
                    title="Галерея">Галерея</a>
@@ -72,33 +76,33 @@
                 <a href="{{route('user-replay.create',['id' => auth()->id()])}}"
                    title="Отправить реплей">Отправить реплей</a>
             </li>
-{{--            <li>--}}
-{{--                <a href="{{route('galleries.index')}}"--}}
-{{--                   title="Галерея">Галерея</a>--}}
-{{--            </li>--}}
+            {{--            <li>--}}
+            {{--                <a href="{{route('galleries.index')}}"--}}
+            {{--                   title="Галерея">Галерея</a>--}}
+            {{--            </li>--}}
             <li>
                 <a href="{{route('edit_profile',['id' => auth()->id()])}}"
                    title="Настройки">Настройки</a>
             </li>
-{{--            <li>--}}
-{{--                <a href="#" title="Мои стримы">Мои стримы</a>--}}
-{{--            </li>--}}
-{{--            <li>--}}
-{{--                <a href="{{route('user-topics.index',['id' => auth()->id()]) }}"--}}
-{{--                   title="Моя темы">Мои темы</a>--}}
-{{--            </li>--}}
-{{--            <li>--}}
-{{--                <a href="{{route('user-comments.index',['id' => auth()->id()]) }}"--}}
-{{--                   title="Мои комментарии">Мои комментарии</a>--}}
-{{--            </li>--}}
-{{--            <li>--}}
-{{--                <a href="{{route('user-replay.index',['id' => auth()->id(),'type'=>'user'])}}"--}}
-{{--                   title="Мои пользовательские реплеи">Мои пользовательские реплеи</a>--}}
-{{--            </li>--}}
-{{--            <li>--}}
-{{--                <a href="{{route('user-replay.index',['id' => auth()->id(),'type'=>'pro'])}}"--}}
-{{--                   title="Мои профессиональные реплеи">Мои профессиональные реплеи</a>--}}
-{{--            </li>--}}
+            {{--            <li>--}}
+            {{--                <a href="#" title="Мои стримы">Мои стримы</a>--}}
+            {{--            </li>--}}
+            {{--            <li>--}}
+            {{--                <a href="{{route('user-topics.index',['id' => auth()->id()]) }}"--}}
+            {{--                   title="Моя темы">Мои темы</a>--}}
+            {{--            </li>--}}
+            {{--            <li>--}}
+            {{--                <a href="{{route('user-comments.index',['id' => auth()->id()]) }}"--}}
+            {{--                   title="Мои комментарии">Мои комментарии</a>--}}
+            {{--            </li>--}}
+            {{--            <li>--}}
+            {{--                <a href="{{route('user-replay.index',['id' => auth()->id(),'type'=>'user'])}}"--}}
+            {{--                   title="Мои пользовательские реплеи">Мои пользовательские реплеи</a>--}}
+            {{--            </li>--}}
+            {{--            <li>--}}
+            {{--                <a href="{{route('user-replay.index',['id' => auth()->id(),'type'=>'pro'])}}"--}}
+            {{--                   title="Мои профессиональные реплеи">Мои профессиональные реплеи</a>--}}
+            {{--            </li>--}}
         </ul>
     </div>
 
