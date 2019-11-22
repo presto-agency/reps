f9a15123dce5944b2428705c67bace410732b8a8
Sergey <anyshyn@gmail.com>
2019-11-22 21:41:09 +0200

    New ForumSections table

--- app/Http/Sections/ForumSections.php
+++ app/Http/Sections/ForumSections.php
@@ -8,10 +8,7 @@ use AdminForm;
 use AdminFormElement;
 use SleepingOwl\Admin\Contracts\Display\DisplayInterface;
 use SleepingOwl\Admin\Contracts\Form\FormInterface;
-use SleepingOwl\Admin\Form\Buttons\Cancel;
-use SleepingOwl\Admin\Form\Buttons\Save;
-use SleepingOwl\Admin\Form\Buttons\SaveAndClose;
-use SleepingOwl\Admin\Form\Buttons\SaveAndCreate;
+use SleepingOwl\Admin\Display\ControlLink;
 use SleepingOwl\Admin\Section;
 
 /**
@@ -43,10 +40,7 @@ class ForumSections extends Section
     public function onDisplay()
     {
         $columns = [
-            AdminColumn::text('id', '#')
-                ->setWidth('50px')
-                ->setHtmlAttribute('class', 'text-center'),
-            AdminColumn::text('name', 'Название', 'created_at')
+
             //            ->setSearchCallback(function($column, $query, $search){
             //              return $query->orWhere('name', 'like', '%'.$search.'%')
             //                           ->orWhere('created_at', 'like', '%'.$search.'%');
@@ -54,7 +48,6 @@ class ForumSections extends Section
             //            ->setOrderable(function($query, $direction) {
             //              $query->orderBy('created_at', $direction);
             //            })
-            ,
             //          AdminColumn::boolean('name', 'On'),
             //          AdminColumn::text('created_at', 'Created / updated', 'updated_at')
             //            ->setWidth('160px')
@@ -62,15 +55,31 @@ class ForumSections extends Section
             //              $query->orderBy('updated_at', $direction);
             //            })
             //            ->setSearchable(false),
+            AdminColumn::text('id', '#')->setWidth('50px'),
+            AdminColumn::text('name', 'Название')
+                ->setHtmlAttribute('class', 'text-left'),
+            AdminColumn::text('title', 'Имя'),
+            AdminColumn::text('position', 'Позиция')->setWidth('100px'),
+            AdminColumn::count('topics', 'Количество тем')->setWidth('100px'),
+            \AdminColumnEditable::checkbox('is_active', 'Да', 'Нет')
+                ->setWidth('100px')
+                ->setLabel('Активен'),
+            \AdminColumnEditable::checkbox('is_general', 'Да', 'Нет')
+                ->setWidth('100px')
+                ->setLabel('Основной'),
+            AdminColumn::text('description', 'Описание')
+                ->setHtmlAttribute('class', 'text-left'),
         ];
 
         $display = AdminDisplay::datatables()
             ->setName('forumSectionsTables')
             ->setOrder([[0, 'asc']])
             ->setDisplaySearch(false)
-            ->paginate(25)
+            ->with(['topics'])
+            ->paginate(5)
             ->setColumns($columns)
-            ->setHtmlAttribute('class', 'table-info table-hover th-center');
+            ->setHtmlAttribute('class',
+                'table-info table-hover th-center text-center');
 
 
         //        $display->setColumnFilters([
@@ -86,6 +95,9 @@ class ForumSections extends Section
 
         //        $display->getColumnFilters()->setPlacement('panel.heading');
 
+        $control = $display->getColumns()->getControlColumn();
+        $control->addButton($this->lincShow());
+
         return $display;
     }
 
@@ -96,34 +108,38 @@ class ForumSections extends Section
      */
     public function onEdit($id)
     {
-        $form = AdminForm::panel()->addBody([
-            AdminFormElement::columns()->addColumn([
+        $display = AdminForm::panel();
+        $display->setItems([
+            $name = AdminFormElement::text('name', 'Название:')
+                ->setValidationRules([
+                    'required',
+                    'max:255',
+                ]),
+            $title = AdminFormElement::text('title', 'Имя:')
+                ->setValidationRules([
+                    'required',
+                    'max:255',
+                ]),
+            $position = AdminFormElement::number('position', 'Позиция:')
+                ->setValidationRules([
+                    'required',
+                    'min:0',
+                ]),
+            $description = AdminFormElement::textarea('description',
+                'Описание:')
+                ->setValidationRules([
+                    'required',
+                    'max:255',
+                ]),
+            $isActive = AdminFormElement::checkbox('is_active', 'Активный'),
+            $isGeneral = AdminFormElement::checkbox('is_general', 'Основной'),
+            $userCanAddTopics
+                = AdminFormElement::checkbox('user_can_add_topics',
+                'Пользователь добавляет'),
 
-                AdminFormElement::text('name', 'Name')
-                    ->required(),
-                AdminFormElement::html('<hr>'),
-                AdminFormElement::datetime('created_at')
-                    ->setVisible(true)
-                    ->setReadonly(false),
-                AdminFormElement::html('last AdminFormElement without comma'),
-
-            ], 8)->addColumn([
-
-                AdminFormElement::text('id', 'ID')
-                    ->setReadonly(true),
-                AdminFormElement::html('last AdminFormElement without comma'),
-
-            ]),
         ]);
 
-        $form->getButtons()->setButtons([
-            'save'            => new Save(),
-            'save_and_close'  => new SaveAndClose(),
-            'save_and_create' => new SaveAndCreate(),
-            'cancel'          => (new Cancel()),
-        ]);
-
-        return $form;
+        return $display;
     }
 
     /**
@@ -141,5 +157,19 @@ class ForumSections extends Section
     {
         // remove if unused
     }
+    public function lincShow()
+    {
+        $link = new ControlLink(function ($model) {
+            $url = asset('admin/forum_topics');
 
+            return $url.'?forum_section_id='.$model->getKey();
+        }, function ($model) {
+            //            return $model->title . ' (' . $model->topicsCount() . ')'; // Генерация текста на кнопке
+        }, 50);
+        $link->hideText();
+        $link->setIcon('fa fa-eye');
+        $link->setHtmlAttribute('class', 'btn-info');
+
+        return $link;
+    }
 }
