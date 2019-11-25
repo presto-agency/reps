27f9a071a1eb71842ff74cb544fbe653564074a1
Sergey <anyshyn@gmail.com>
2019-11-25 18:41:55 +0200

    fix stream iframe fis show last 5 replay

--- app/Admin/Policies/UserSectionModelPolicy.php
+++ app/Admin/Policies/UserSectionModelPolicy.php
@@ -2,8 +2,9 @@
 
 
 namespace App\Admin\Policies;
-use App\User;
+
 use App\Http\Sections\User as Section;
+use App\User;
 use App\User as Model;
 use Illuminate\Auth\Access\HandlesAuthorization;
 
@@ -12,79 +13,69 @@ class UserSectionModelPolicy
 
     use HandlesAuthorization;
 
-    public function before(User $user, $ability, Section $section, Model $item = null) {
-                if ($user->superAdminRole()) {
-                    if ($ability != 'display' && ! is_null($item)
-                        && $item->roles->name == 'super-admin'
-                    ) {
-                        return false;
-                    }
-                }
-                if ($user->adminRole()) {
-                    if ($ability != 'display' && ! is_null($item)
-                        && $item->roles->name == 'admin'
-                    ) {
-                        return false;
-                    }
-                    if ($ability != 'display' && ! is_null($item)
-                        && $item->roles->name == 'super-admin'
-                    ) {
-                        return false;
-                    }
-                }
+    public function before(
+        User $user,
+        $ability,
+        Section $section,
+        Model $item = null
+    ) {
 
-        return  true;
-    }
-
-    public function display(User $user, $ability, Section $section, Model $item = null) {
+        //                dd($user->adminRole());
+        //        if ($user->superAdminRole()) {
+        //                    if ($ability != 'display' && ! is_null($item)
+        //                        && $item->roles->name == 'super-admin'
+        //                    ) {
+        //                        return false;
+        //                    }
+        //                }
+        //                if ($user->adminRole()) {
+        //                    if ($ability != 'display' && ! is_null($item)
+        //                        && $item->roles->name == 'admin'
+        //                    ) {
+        //                        return false;
+        //                    }
+        //                    if ($ability != 'display' && ! is_null($item)
+        //                        && $item->roles->name == 'super-admin'
+        //                    ) {
+        //                        return false;
+        //                    }
+        //                }
         return true;
     }
 
-    public function edit(User $user, $ability, Section $section, Model $item = null) {
+    public function display(
+        User $user,
+        $ability,
+        Section $section,
+        Model $item = null
+    ) {
+        return true;
+    }
 
-        if ($user->adminRole()) {
-            if (!is_null($item) && $item->roles->name == 'admin'
-            ) {
-                return false;
-            }
-            if (!is_null($item) && $item->roles->name == 'super-admin'
-            ) {
-                return false;
-            }
-
-        }
+    public function edit(
+        User $user,
+        $ability,
+        Section $section,
+        Model $item = null
+    ) {
         return true;
     }
 
     public function delete(
-        User $user, $ability, Section $section, Model $item = null
+        User $user,
+        $ability,
+        Section $section,
+        Model $item = null
     ) {
-        if ($user->adminRole()) {
-            if (!is_null($item) && $item->roles->name == 'admin'
-            ) {
-                return false;
-            }
-            if (!is_null($item) && $item->roles->name == 'super-admin'
-            ) {
-                return false;
-            }
-
-        }
         return true;
     }
 
-    public function create(User $user, $ability, Section $section, Model $item = null) {
-        if ($user->adminRole()) {
-            if (!is_null($item) && $item->roles->name == 'admin'
-            ) {
-                return false;
-            }
-            if (!is_null($item) && $item->roles->name == 'super-admin'
-            ) {
-                return false;
-            }
-
-        }
+    public function create(
+        User $user,
+        $ability,
+        Section $section,
+        Model $item = null
+    ) {
         return true;
     }
 
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -24,7 +24,7 @@ class DatabaseSeeder extends Seeder
          * reps.(<replay_maps>)
          * loc.(<replays>,<replay_maps>).
          */
-                $this->call(TransferReplayMaps::class);
+//                $this->call(TransferReplayMaps::class);
 
         /**
          *
@@ -37,7 +37,7 @@ class DatabaseSeeder extends Seeder
          * reps.(<tourney_lists>,<files>)
          * loc.(<tourney_lists>).
          */
-                $this->call(TransferTournamentsList::class);
+//                $this->call(TransferTournamentsList::class);
 
         /**
          *
@@ -50,7 +50,7 @@ class DatabaseSeeder extends Seeder
          * reps.(<tourney_matches>,<files>)
          * loc.(<tourney_matches>).
          */
-                $this->call(TransferTournamentsMatches::class);
+//                $this->call(TransferTournamentsMatches::class);
 
         /**
          *
@@ -63,7 +63,7 @@ class DatabaseSeeder extends Seeder
          * reps.(<tourney_players>)
          * loc.(<tourney_players>).
          */
-                $this->call(TransferTournamentsPlayers::class);
+//                $this->call(TransferTournamentsPlayers::class);
 
         /**
          *
@@ -77,7 +77,7 @@ class DatabaseSeeder extends Seeder
          * reps.(<countries>)
          * loc.(<countries>,<replays>,<users>,<streams>).
          */
-                $this->call(TransferCountries::class);
+//                $this->call(TransferCountries::class);
 
 
         /**
@@ -92,7 +92,7 @@ class DatabaseSeeder extends Seeder
          * reps.(<user_activity_logs>)
          * loc.(<user_activity_logs>,<users>).
          */
-                $this->call(TransferUserActivityLogs::class);
+//                $this->call(TransferUserActivityLogs::class);
 
         /**
          *
@@ -105,7 +105,7 @@ class DatabaseSeeder extends Seeder
          * reps.(<replays>,<files>)
          * loc.(<replays>,<races>).
          */
-                $this->call(TransferReplays::class);
+//                $this->call(TransferReplays::class);
 
         /**
          *
@@ -119,8 +119,8 @@ class DatabaseSeeder extends Seeder
          * reps.(<streams>)
          * loc.(<streams>,<races>).
          */
-        $this->call(TransferStreams::class);
-        $this->call(SeederStreams::class);
+//        $this->call(TransferStreams::class);
+//        $this->call(SeederStreams::class);
         /**
          *
          * Transfer data from reps reps.<forum_sections> in loc.<forum_sections>
@@ -132,7 +132,7 @@ class DatabaseSeeder extends Seeder
          * reps.(<forum_sections>)
          * loc.(<forum_sections>).
          */
-                $this->call(TransferForumSections::class);
+//                $this->call(TransferForumSections::class);
 
         /**
          *
@@ -145,7 +145,7 @@ class DatabaseSeeder extends Seeder
          * reps.(<forum_topics>,<files>)
          * loc.(<forum_topics>).
          */
-                $this->call(TransferForumTopics::class);
+//                $this->call(TransferForumTopics::class);
 
         /**
          *
@@ -159,7 +159,7 @@ class DatabaseSeeder extends Seeder
          * loc.(<users>,<races>,<countries>,<roles>).
          */
         //        $this->call(TransferUsers::class);
-        //        $this->call(SeederSuperAdmin::class);
+                $this->call(SeederSuperAdmin::class);
 
         /**
          *
@@ -189,7 +189,7 @@ class DatabaseSeeder extends Seeder
          * reps.(<interview_questions>,<interview_user_answers>,<interview_variants_answers>)
          * loc.(<interview_questions>,<interview_user_answers>,<interview_variants_answers>).
          */
-                $this->call(TransferInterview::class);
+//                $this->call(TransferInterview::class);
         /**
          *
          * Transfer data from reps reps.<user_reputations> in loc.<user_reputations>
@@ -201,7 +201,7 @@ class DatabaseSeeder extends Seeder
          * reps.(<user_reputations>)
          * loc.(<user_reputations>).
          */
-                $this->call(TransferUserReputations::class);
+//                $this->call(TransferUserReputations::class);
 
         /**
          *
@@ -215,7 +215,7 @@ class DatabaseSeeder extends Seeder
          * reps.(<user_galleries>)
          * loc.(<user_galleries>).
          */
-                $this->call(TransferUserGalleries::class);
+//                $this->call(TransferUserGalleries::class);
     }
 
 }
