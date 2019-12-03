7411f67398dfaf0a55553fe53d0341e87942eeab
Sergey <anyshyn@gmail.com>
2019-12-03 13:16:40 +0200

    fix create 5

--- app/Http/Sections/Country.php
+++ app/Http/Sections/Country.php
@@ -133,7 +133,28 @@ class Country extends Section
     public function onCreate()
     {
         \Log::info('hi');
-        return $this->onEdit(null);
+        $form = AdminForm::panel();
+        $form->setItems(
+            AdminFormElement::columns()
+                ->addColumn(function () {
+                    return [
+                        AdminFormElement::text('name', 'Название')
+                            ->setHtmlAttribute('placeholder', 'Название')
+                            ->setValidationRules([
+                                'required', 'min:2', 'max:255',
+                            ]),
+                        AdminFormElement::text('code', 'Код страны')
+                            ->setHtmlAttribute('placeholder', 'Код страны')
+                            ->setHtmlAttribute('title', 'Alpha-2 ISO 3166-1')
+                            ->setValidationRules([
+                                'required', 'min:2', 'max:10',
+                            ]),
+                    ];
+                })
+        );
+
+        return $form;
+
     }
 
     /**
