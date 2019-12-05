96cdde890af0167b43bacd57f84076f6475fb43b
Sergey <anyshyn@gmail.com>
2019-12-05 16:40:55 +0200

    add phpinfo rout2

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,9 @@
 Auth::routes([
     'verify' => true,
 ]);
-
-Route::get('/php-info',function (){
-    return phpinfo();
-});
+//Route::get('/php-info',function (){
+//    return phpinfo();
+//});
 /***---Home---***/
 Route::get('/', 'HomeController@index')->name('home.index');
 
