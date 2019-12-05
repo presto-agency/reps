6e03fe57fbe2252419d8c8db033d866c847ec479
Sergey <anyshyn@gmail.com>
2019-12-05 19:00:38 +0200

    add cache drive redis

--- config/cache.php
+++ config/cache.php
@@ -18,7 +18,8 @@ return [
     |
     */
 
-    'default' => env('CACHE_DRIVER', 'file'),
+//    'default' => env('CACHE_DRIVER', 'file'),
+    'default' => 'redis',
 
     /*
     |--------------------------------------------------------------------------
