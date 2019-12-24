767130b243e390dd6df7eb41463bcd64764e24ec
Alexandr <basok.sanya@gmail.com>
2019-12-24 16:14:14 +0200

    chat impovement2

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -104,9 +104,7 @@ class ChatController extends Controller
             'time'         => $msg->created_at->format('H:i'),
             'country_flag' => $country_flag,
             'is_hidden'    => $msg->is_hidden,
-            //'avatar'       => $msg->user->avatarOrDefault()??'images/default/avatar/avatar.png',
             'avatar'       => isset($msg->user->avatar) ? '/'.$msg->user->avatar : 'images/default/avatar/avatar.png',
-            'user' => $msg->user
         ];
     }
 
