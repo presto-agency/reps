7fec71588a08013b0af6edc45e9b0384f8ba6f9e
Sergey <anyshyn@gmail.com>
2020-02-10 12:19:17 +0200

    small fix for tourney

--- app/Http/Sections/TournamentsMapPool.php
+++ app/Http/Sections/TournamentsMapPool.php
@@ -125,7 +125,7 @@ class TournamentsMapPool extends Section
                 ->setValidationRules([
                     'exists:tourney_lists,id',
                 ])
-                ->setOptions((new \App\Models\TourneyList())->whereNotIn('status', [3, 4, 5])->pluck('name', 'id')->toArray())
+                ->setOptions((new \App\Models\TourneyList())->whereNotIn('status', [5, 6])->pluck('name', 'id')->toArray())
                 ->setHelpText('Все турниры со статусами:(ANNOUNCE;REGISTRATION;CHECK-IN)')
                 ->setLabel('Турнир'),
         ]);
@@ -149,7 +149,7 @@ class TournamentsMapPool extends Section
                     'required',
                     'exists:tourney_lists,id',
                 ])
-                ->setOptions((new \App\Models\TourneyList())->whereNotIn('status', [3, 4, 5])->pluck('name', 'id')->toArray())
+                ->setOptions((new \App\Models\TourneyList())->whereNotIn('status', [5, 6])->pluck('name', 'id')->toArray())
                 ->setHelpText('Все турниры со статусами:(ANNOUNCE;REGISTRATION;CHECK-IN)')
                 ->setLabel('Турнир'),
         ]);
