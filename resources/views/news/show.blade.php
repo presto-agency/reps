faf95dc7b8b7b74c7fbff2d3ae731fc46392222f
Sergey <anyshyn@gmail.com>
2020-05-26 22:44:17 +0300

    ADD meta tags update favicon

--- resources/views/news/show.blade.php
+++ resources/views/news/show.blade.php
@@ -7,36 +7,20 @@
 @endphp
 
 @if(!is_null($seo_title))
-    @section('meta-title')
-        {{$seo_title}}
-    @endsection
-    @section('meta-og-title')
-        {{$seo_title}}
-    @endsection
+    @section('meta-title'){{$seo_title}}@endsection
+    @section('meta-og-title'){{$seo_title}}@endsection
 @endif
 @if(!is_null($seo_keywords))
-    @section('meta-keywords')
-        {{$seo_keywords}}
-    @endsection
-    @section('meta-og-keywords')
-        {{$seo_keywords}}
-    @endsection
+    @section('meta-keywords'){{$seo_keywords}}@endsection
+    @section('meta-og-keywords'){{$seo_keywords}}@endsection
 @endif
 @if(!is_null($seo_description))
-    @section('meta-description')
-        {{$seo_description}}
-    @endsection
-    @section('meta-og-description')
-        {{$seo_description}}
-    @endsection
+    @section('meta-description'){{$seo_description}}@endsection
+    @section('meta-og-description'){{$seo_description}}@endsection
 @endif
 @if(!is_null($seo_og_icon))
-    @section('meta-og-image')
-        {{$seo_og_icon}}
-    @endsection
-    @section('meta-og-image_url')
-        {{$seo_og_icon}}
-    @endsection
+    @section('meta-og-image'){{asset($seo_og_icon)}}@endsection
+    @section('meta-og-image_url'){{asset($seo_og_icon)}}@endsection
 @endif
 
 
