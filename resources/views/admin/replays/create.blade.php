985239aaee451417802e9c249ecab7b882790123
Sergey <anyshyn@gmail.com>
2020-06-15 19:27:20 +0300

    REPS-449 twitch

--- resources/views/admin/replays/create.blade.php
+++ resources/views/admin/replays/create.blade.php
@@ -1,7 +1,7 @@
 <div class="form-group">
-    <label for="video_iframe_url" class="night_text">{{__('Вставте URL для Video Iframe')}}</label>
+    <label for="video_iframe_url" class="night_text">{{__('Вставить URL для Video Iframe')}}</label>
     <input id="video_iframe_url" name="video_iframe_url" class="form-control night_input" maxlength="500"
-           placeholder="{{__('Вставте URL для Video Iframe')}}"
+           placeholder="{{__('Вставить URL для Video Iframe')}}"
            data-url="{{route('set.iframe')}}"
            value="">
 </div>
--- resources/views/home/components/chat/components/streams-list.blade.php
+++ resources/views/home/components/chat/components/streams-list.blade.php
@@ -29,31 +29,5 @@
 </section>
 @section('custom-script')
     @parent
-    <script type="text/javascript" defer>
-        $(document).ready(function () {
-            const div = $('#streamOnline');
-            const divName = $('#streamOnlineName');
-            const divRace = $('#streamOnlineRace');
-            const divFlag = $('#streamOnlineFlag');
-            let element = document.getElementById("1");
-            div.attr('src', element.getAttribute('data-src'));
-            divFlag.attr('src', element.getAttribute('data-img-flag'));
-            divFlag.attr('title', element.getAttribute('data-name-flag'));
-            divRace.attr('src', element.getAttribute('data-img-race'));
-            divRace.attr('title', element.getAttribute('data-title-race'));
-            divName.attr('title', element.getAttribute('data-stream-title'));
-            divName.text(element.getAttribute('data-stream-title'));
-
-            $('.streamEvent').click(function () {
-                div.attr('src', $(this).data('src'));
-                divFlag.attr('src', $(this).data('img-flag'));
-                divFlag.attr('title', $(this).data('name-flag'));
-                divRace.attr('src', $(this).data('img-race'));
-                divRace.attr('title', $(this).data('title-race'));
-                divName.attr('title', $(this).data('stream-title'));
-                divName.text($(this).data('stream-title'));
-            });
-        });
-
-    </script>
+    <script type="text/javascript" src="{{ mix('js/assets/stream_select.js') }}" defer></script>
 @endsection
