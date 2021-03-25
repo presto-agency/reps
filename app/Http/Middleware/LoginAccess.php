ee12d69b087732a4e63deb47d9b98e31c156ecbf
Sergey <anyshyn@gmail.com>
2021-03-25 10:06:02 +0200

    fix register and some upd title

--- app/Http/Middleware/LoginAccess.php
+++ app/Http/Middleware/LoginAccess.php
@@ -18,19 +18,18 @@ class LoginAccess
      */
     public function handle($request, Closure $next)
     {
-        $user = null;
         if ($request->email) {
             $user = User::where('email', $request->email)->first();
+            if ($user && $user->ban) {
+                \Session::flash('showModal', 'ban');
+                return back();
+            }
+            if ($user && is_null($user->email_verified_at)) {
+                \Session::flash('showModal', 'no_email_confirm');
+                return back();
+            }
         }
 
-        if ($user && $user->ban) {
-            \Session::flash('showModal', 'ban');
-            return back();
-        }
-        if ($user && is_null($user->email_verified_at)) {
-            \Session::flash('showModal', 'no_email_confirm');
-            return back();
-        }
         if (auth()->check() && auth()->user()->ban) {
             auth()->logout();
             \Session::flash('showModal', 'ban');
